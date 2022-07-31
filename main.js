51b648a4eae892b58ed406b77cafd3ef2bcbc79f
nik.m <nik.m@investment.com.au>
2022-07-31 21:35:37 +1000

    Add homework page and fix login action

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-function login ()  {
-const login = document.getElementById ("login")
-const pass = document.getElementById ("password")
-if (login == 0 && pass == 0) {
-    window.location.href = "/"
+function login() {
+    const login = document.getElementById("login")
+    const password = document.getElementById("password")
+    if (login.value === '0' && password.value === '0') {
+        window.location.href = "/english-platform/main-page.html"
+    }
 }
-console.log (login)
+
+function getHomework() {
+    const url = 'https://jsonplaceholder.typicode.com/todos';
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            renderHomeworkList(data.splice(0, 10));
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
+
+function renderHomeworkList(items) {
+    let listElement = document.getElementById('homework-list');
+    items.forEach((homework) => {
+        let item = document.createElement('li');
+        item.innerText = homework.title;
+        item.className = 'homework-item';
+        listElement.appendChild(item);
+    })
 }
